3968ad7bf7f8dfae55836cefc7950e8f5f2ef04c
TechnoTuition <80044338+TechnoTuition@users.noreply.github.com>
2024-02-13 19:31:53 +0530

    api
    api section add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 import requests
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,jsonify
 
 API_key = "c56326a851cc41eba5e170519230504&q"
 
@@ -35,6 +35,23 @@ def get_detail(cityName):
 
     return render_template("showdetail.html", data=data)
 
+
+@app.route("/docs")
+def api_docs():
+    return render_template("Apidocs.html")
+
+
+User_api_url = f"https://surajprajapati.pythonanywhere.com/api"
+@app.get("/api")
+def api_request():
+
+    city = request.args.get('cityname')
+    URL = f' https://api.weatherapi.com/v1/current.json?key={API_key}={city}'
+    r = requests.get(URL)
+
+    data = r.json()
+    print(r.json())
+    return jsonify(r.json())
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True, host="0.0.0.0")
